1179c6132af66d695e47d18951964aaf566cc514
Caio <caio_nms@hotmail.com>
2017-11-12 03:47:31 +0000

    Handler Venda

---
+++ src/Handler/Venda.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Handler.Venda where
+
+import Import
+import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+import GHC.Generics
