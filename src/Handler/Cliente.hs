9f2f5e5a219ba38f367e241dc3acfdd3e8ea8c30
Caio <caio_nms@hotmail.com>
2017-11-13 03:38:24 +0000

    Ajuste no Handler Cliente para se adequar às rotas

--- src/Handler/Cliente.hs
+++ src/Handler/Cliente.hs
@@ -3,32 +3,31 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Handler.Cliente where
 
 import Import
-import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 import GHC.Generics
 
 
-getLoginCliR = Handler TypedContent
+getLoginCliR :: Text -> Text -> Handler TypedContent
 getLoginCliR = undefined
 
 
-postCadastroCLiR = Handler TypedContent
-postCadastroCLiR = undefined
+postCadastroCliR :: Handler TypedContent
+postCadastroCliR = undefined
 
-putAlterarDadosCliR = ClienteId -> Handler Value
+putAlterarDadosCliR :: ClienteId ->Handler Value
 putAlterarDadosCliR cid = do
-        	_ <- runDB $ get404 cid
-			novoProd <- requireJsonBody :: Handler Cliente
-			runDB $ replace cid novoCli
-			sendStatusJSON noContent204 (object ["resp" .=("Updated" ++ show (fromSqlKey cid))])
+	_ <- runDB $ get404 cid
+	novoCli <- requireJsonBody :: Handler Cliente
+	runDB $ replace cid novoCli
+	sendStatusJSON noContent204 (object ["resp" .=("Updated" ++ show (fromSqlKey cid))])
 
-getCarrinhoComprasR = Handler TypeFamilies
+getCarrinhoComprasR :: Handler TypedContent
 getCarrinhoComprasR = undefined
 
-
-postFinalizarCompraR = Handler TypedContent
+postFinalizarCompraR :: Handler TypedContent
 postFinalizarCompraR = undefined
