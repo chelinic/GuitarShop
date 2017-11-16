440e5f13be8912987b8e2fd8e79ce55216f0f879
Caio <caio_nms@hotmail.com>
2017-11-16 02:53:57 +0000

    Handler Home- remoção temporária de CSS

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -13,81 +13,43 @@ import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = do 
-	defaultLayout $ do
-	    addStylesheet $ (StaticR css_home_css)
-	    addStylesheet $ (StaticR css_bootstrap_css)
-	    $(whamletFile "templates/Home.hamlet")
-    
+getHomeR = do
+    defaultLayout $ do
+       --addStylesheet $(StaticR css_home_css)
+	    $(whamletFile "templates/Home.whamlet")
+
 getRecemChegadosR :: Handler Html
 getRecemChegadosR = do
     defaultLayout $ do
-        toWidget $ [lucius|
-            h1 {
-                color: blue;
-            }
-        |]
-        [whamlet|
-            <h1> Guitarras
-        |]
+    --	addStylesheet $ (StaticR css_recemchegados_css)
+    	$(whamletFile "templates/RecemChegados.whamlet")
         
 getGuitarrasR :: Handler Html
 getGuitarrasR = do
     defaultLayout $ do
-        toWidget $ [lucius|
-            h1 {
-                color: blue;
-            }
-        |]
-        [whamlet|
-            <h1> Guitarras
-        |]
-        
-
+     --   addStylesheet $ (StaticR css_guitarras_css)
+        $(whamletFile "templates/Guitarras.whamlet")
+	    
 getBaixosR :: Handler Html
 getBaixosR = do
     defaultLayout $ do
-        toWidget $ [lucius|
-            h1{
-                color: blue;
-            }
-        |]
-        [whamlet|
-            <h1> Baixos
-        |]
+       -- addStylesheet $ (StaticR css_baixos_css)
+        $(whamletFile "templates/Baixos.whamlet")
 
 getPedaisR :: Handler Html
 getPedaisR = do
     defaultLayout $ do
-        toWidget $ [lucius|
-            h1{
-                color: blue;
-            }
-        |]
-        [whamlet|
-            <h1> Pedais
-        |]
+        --addStylesheet $ (StaticR css_home_css)
+        $(whamletFile "templates/Pedais.whamlet")
 
 getAmplificadoresR :: Handler Html
 getAmplificadoresR = do
     defaultLayout $ do
-        toWidget $ [lucius|
-            h1{
-                color: blue;
-            }
-        |]
-        [whamlet|
-            <h1> AmplificadoresR
-        |]
+    	--addStylesheet $ (StaticR css_amplificadores_css)
+    	$(whamletFile "templates/Amplificadores.whamlet")
 
 getAcessoriosR :: Handler Html
 getAcessoriosR = do
     defaultLayout $ do
-        toWidget $ [lucius|
-            h1{
-                color: blue;
-            }
-        |]
-        [whamlet|
-            <h1> Acessorios
-        |]
+        --addStylesheet $ (StaticR css_home_css)
+	    $(whamletFile "templates/Acessorios.whamlet")
