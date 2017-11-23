644fc3b1f6deb7b9e65bcff593b5112c7c47bec8
Caio <caio_nms@hotmail.com>
2017-11-23 17:13:20 +0000

    Handler Cliente - Login e autenticação

--- src/Handler/Cliente.hs
+++ src/Handler/Cliente.hs
@@ -11,22 +11,68 @@ import Import
 import Database.Persist.Postgresql
 import GHC.Generics
 
-formCliente :: Form Cliente
-formCliente = renderBootstrap $ Cliente
+formCadCliente :: Form Cliente
+formCadCliente = renderBootstrap $ Cliente
     <$> areq textField "Nome:" Nothing
     <*> areq textField "CPF:" Nothing
     <*> areq textField "Telefone:" Nothing
     <*> areq textField "Email:" Nothing
     <*> areq textField "Senha:" Nothing
     
-    
-getLoginCliR :: Text -> Text -> Handler TypedContent
-getLoginCliR = undefined
+formLoginCli :: Form (Text, Text)
+-- Form com dois campos de texto
+formLoginCli = renderDivs $ (,)
+    -- (,) 5 7 = (5,7)
+    <$> areq textField "Email: " Nothing
+    <*> areq passwordField "Senha: " Nothing   
+
+autenticar :: Text -> Text -> HandlerT App IO (Maybe (Entity Cliente))
+    -- HandlerT é monad transform
+    -- App é o site
+    -- o objetivo é transformar IO em um Handler
+    -- retorna dentro do handler um Nothing ou um Just Aluno
+    -- 2 Mônadas: Handler e Maybe
+    -- Entity é TypeFamilies: transforma o Maybe a em Maybe Aluno
+autenticar email senha = runDB $ selectFirst [ClienteEmailcli ==. email,
+                                              ClienteSenha ==. senha][]
+                                            -- a ","" é o AND
+                                            -- primeira lista é condição WHERE
+
+getLoginCliR :: Handler Html
+getLoginCliR = do
+    (widget, enctype) <- generateFormPost formLoginCli
+    msg <- getMessage
+    defaultLayout $ do
+        [whamlet|
+            $maybe mensa <- msg
+                <h1> Usuario Invalido
+            <form action=@{LoginCliR} method=post>
+                ^{widget}
+                <input type="submit" value="Login">
+        |]
+
+postLoginCliR :: Handler Html
+postLoginCliR = do
+    ((res,_),_) <- runFormPost formLoginCli
+    -- res é o resultado
+    case res of
+        FormSuccess (email,senha) -> do
+        -- se deu certo vai retornar um aluno
+            cli <- autenticar email senha
+            case cli of
+                Nothing -> do
+                    setMessage [shamlet| Usuario ou senha invalido|]
+                    -- hamlet sem w
+                    redirect LoginCliR
+                Just (Entity clienteid cli) -> do
+                    setSession "_ID" (clienteNome cli)
+                    redirect LoginCliR
+        _ -> redirect HomeR
 
 
 postCadastroCliR :: Handler Html
 postCadastroCliR = do
-    ((result,_),_) <- runFormPost formCliente
+    ((result,_),_) <- runFormPost formCadCliente
     case result of
         FormSuccess cliente -> do
             runDB $ insert cliente
@@ -38,7 +84,7 @@ postCadastroCliR = do
         
 getCadastroCliR :: Handler Html
 getCadastroCliR = do
-    (widget, enctype) <- generateFormPost formCliente
+    (widget, enctype) <- generateFormPost formCadCliente
     defaultLayout $ do
         addStylesheet $ (StaticR css_bootstrap_css)
         [whamlet|
@@ -65,3 +111,9 @@ getCarrinhoComprasR = undefined
 
 postFinalizarCompraR :: Handler TypedContent
 postFinalizarCompraR = undefined
+
+
+postCliLogoutR :: Handler Html
+postCliLogoutR = do 
+    deleteSession "_ID"
+    redirect HomeR
