31400e39df1eafb27b9be5b9d382e69f2d3d8218
Caio <caio_nms@hotmail.com>
2017-11-27 17:03:46 +0000

    Organização da chamada de templates

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -11,60 +11,132 @@ module Handler.Home where
 import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import Data.Time.Clock
+import Data.Time.Calendar
+
+footer :: WidgetT App IO ()  
+footer = $(whamletFile "templates/Footer.hamlet")
+
+home :: WidgetT App IO ()  
+home = $(whamletFile "templates/Home.hamlet")
+
+prods :: WidgetT App IO ()
+prods = $(whamletFile "templates/Produto.hamlet")
+
+
+
 
 getHomeR :: Handler Html
 getHomeR = do
     clientelog <- lookupSession "_ID"
     defaultLayout $ do
-       --addStylesheet $(StaticR css_home_css)
-	    $(whamletFile "templates/Home.whamlet")
-	     [whamlet|
-            $maybe aluno <- alunologado
-                <h1> _{MsgBemvindo} - #{aluno}
+        [whamlet|
+            $maybe cli <- clientelog
+                <h1> _{MsgBemvindo} - #{Cliente}
             $nothing
                 <h1> _{MsgBemvindo} - _{MsgVisita}
             <ul>
-                <li> <a href=@{AlunoR}> Cadastro de Usuario
+                <li> <a href=@{CadastroCliR}> Cadastro de Cliente
                 $maybe cli <- clientelog
-                    <form action=@{LogoutCliR} method=post>
+                    <form action=@{CliLogoutR} method=post>
                         <input type="submit" value="Logout">
                     -- se tiver logado, faz o logout
                 $nothing
                     <li> <a href=@{LoginCliR}> Login
+            ^{home}
+            ^{footer}
         |]
+
 
 getRecemChegadosR :: Handler Html
 getRecemChegadosR = do
+    now <- getCurrentTime
+    let (year, month, day) = toGregorian $ utctDay now
     defaultLayout $ do
     --	addStylesheet $ (StaticR css_recemchegados_css)
-    	$(whamletFile "templates/RecemChegados.whamlet")
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]
         
 getGuitarrasR :: Handler Html
 getGuitarrasR = do
     defaultLayout $ do
      --   addStylesheet $ (StaticR css_guitarras_css)
-        $(whamletFile "templates/Guitarras.whamlet")
+        
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]
 	    
 getBaixosR :: Handler Html
 getBaixosR = do
     defaultLayout $ do
        -- addStylesheet $ (StaticR css_baixos_css)
-        $(whamletFile "templates/Baixos.whamlet")
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]        
 
 getPedaisR :: Handler Html
 getPedaisR = do
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
-        $(whamletFile "templates/Pedais.whamlet")
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]        
 
 getAmplificadoresR :: Handler Html
 getAmplificadoresR = do
     defaultLayout $ do
     	--addStylesheet $ (StaticR css_amplificadores_css)
-    	$(whamletFile "templates/Amplificadores.whamlet")
+    	[whamlet|
+            ^{prods}
+            ^{footer}
+        |]
 
 getAcessoriosR :: Handler Html
 getAcessoriosR = do
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
-	    $(whamletFile "templates/Acessorios.whamlet")
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]	    
+	    
+getVioloesR :: Handler Html
+getVioloesR = do
+    defaultLayout $ do
+        --addStylesheet $ (StaticR css_home_css)
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]	    
+	 
+getVintageR :: Handler Html
+getVintageR = do
+    defaultLayout $ do
+        --addStylesheet $ (StaticR css_home_css)
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]	    
+	    
+getNovosR :: Handler Html
+getNovosR = do
+    defaultLayout $ do
+        --addStylesheet $ (StaticR css_home_css)
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]	    
+	    
+getUsadosR :: Handler Html
+getUsadosR = do
+    defaultLayout $ do
+        --addStylesheet $ (StaticR css_home_css)
+        [whamlet|
+            ^{prods}
+            ^{footer}
+        |]	 
