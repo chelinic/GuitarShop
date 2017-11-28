ac57cf10bea4a248b6de66b5fc450d9d48d6a556
Caio <caio_nms@hotmail.com>
2017-11-28 02:05:46 +0000

    Handler Home - Queries dos menus de busca de produto

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -13,6 +13,9 @@ import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 import Data.Time.Clock
 import Data.Time.Calendar
+import Database.Esqueleto.PostgreSQL
+import qualified Database.Esqueleto      as E
+import Database.Esqueleto   ((^.))
 
 footer :: WidgetT App IO ()  
 footer = $(whamletFile "templates/Footer.hamlet")
@@ -22,8 +25,6 @@ home = $(whamletFile "templates/Home.hamlet")
 
 prods :: WidgetT App IO ()
 prods = $(whamletFile "templates/Produto.hamlet")
-
-
 
 
 getHomeR :: Handler Html
@@ -48,19 +49,66 @@ getHomeR = do
         |]
 
 
+
 getRecemChegadosR :: Handler Html
 getRecemChegadosR = do
-    now <- getCurrentTime
-    let (year, month, day) = toGregorian $ utctDay now
+    hoje <- getCurrentTime 
+    --let (year, month, day) = toGregorian $ utctDay now
     defaultLayout $ do
     --	addStylesheet $ (StaticR css_recemchegados_css)
         [whamlet|
             ^{prods}
+            listaprods <- runDB $ selectList [ProdutoDtChegada <=. (hoje-7)] []
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td> Id
+                            <td> Nome 
+                            <td> Estoqueminimo
+                            <td> Estoqueatual
+                            <td> Produto
+                            <td>  
+                
+                    <tbody>
+                        $forall (Entity idproduto produto) <- listaprods
+                            <tr> 
+                                <td> #{fromSqlKey idproduto}
+                                <td> #{produtoNome produto}
+                                <td> #{produtoEstoqueatual produto}
+                                <td> #{produtoEstoqueminimo produto}
+                                <td> 
+       
             ^{footer}
         |]
         
+{--
+Query Join
+
+getListaProdFR :: ClienteId -> Handler TypedContent
+getListaProdFR cid = do 
+    lista' <- runDB $ selectList [CompraCliid ==. cid] []
+    lista <- return $ fmap (\(Entity _ comp) -> comp) lista'
+    prodsIds <- return $ fmap compraProid lista
+    produtos <- sequence $ fmap (\pid -> runDB $ get404 pid) prodsIds
+    sendStatusJSON ok200 (object ["resp" .= (toJSON produtos)])
+    
+--}
+
 getGuitarrasR :: Handler Html
 getGuitarrasR = do
+    let listaprods = runDB $ E.select
+       $ E.from $ \(Produto `E.InnerJoin` TipoProd) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. TipoProdnome != val "Guitarra"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
      --   addStylesheet $ (StaticR css_guitarras_css)
         
@@ -71,6 +119,18 @@ getGuitarrasR = do
 	    
 getBaixosR :: Handler Html
 getBaixosR = do
+    let listaprods = runDB $ E.select
+       $ E.from $ \(Produto `E.InnerJoin` TipoProd) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. TipoProdnome != val "Baixo"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
        -- addStylesheet $ (StaticR css_baixos_css)
         [whamlet|
@@ -80,6 +140,18 @@ getBaixosR = do
 
 getPedaisR :: Handler Html
 getPedaisR = do
+    let listaprods = runDB $ E.select
+       $ E.from $ \(Produto `E.InnerJoin` TipoProd) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. TipoProdnome != val "Pedal"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
         [whamlet|
@@ -89,6 +161,18 @@ getPedaisR = do
 
 getAmplificadoresR :: Handler Html
 getAmplificadoresR = do
+    let listaprods = runDB $ E.select
+       $ E.from $ \(Produto `E.InnerJoin` TipoProd) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. TipoProdnome != val "Amplificador"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
     	--addStylesheet $ (StaticR css_amplificadores_css)
     	[whamlet|
@@ -98,6 +182,18 @@ getAmplificadoresR = do
 
 getAcessoriosR :: Handler Html
 getAcessoriosR = do
+    let listaprods = runDB $ E.select
+        $ E.from $ \(Produto `E.InnerJoin` TipoProd) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. TipoProdnome != val "Acessorio"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
         [whamlet|
@@ -107,6 +203,18 @@ getAcessoriosR = do
 	    
 getVioloesR :: Handler Html
 getVioloesR = do
+    let listaprods = runDB $ E.select
+        $ E.from $ \(Produto `E.InnerJoin` TipoProd) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. TipoProdnome != val "Violão"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
         [whamlet|
@@ -116,6 +224,18 @@ getVioloesR = do
 	 
 getVintageR :: Handler Html
 getVintageR = do
+    let listaprods = runDB $ E.select
+        $ E.from $ \(Produto `E.InnerJoin` EstadoUso) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. EstadoUsonome != val "Novo"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
         [whamlet|
@@ -125,6 +245,18 @@ getVintageR = do
 	    
 getNovosR :: Handler Html
 getNovosR = do
+    let listaprods = runDB $ E.select
+        $ E.from $ \(Produto `E.InnerJoin` EstadoUso) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. EstadoUsonome != val "Novo"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
         [whamlet|
@@ -134,6 +266,18 @@ getNovosR = do
 	    
 getUsadosR :: Handler Html
 getUsadosR = do
+    let listaprods = runDB $ E.select
+       $ E.from $ \(Produto `E.InnerJoin` EstadoUso) -> do
+            E.on $ Produto ^. ProdutotipoprodID E.==. TipoProd ^. TipoProdId
+                E.where_ $
+                    E. EstadoUsonome != val "Usado"
+                return
+                    ( Produto ^. ProdutoId
+                    , Produto ^. ProdutoNome
+                    , Produto ^. ProdutoEstoqueAtual
+                    , EstadoUso ^. EstadoUsonome
+                    
+                    )
     defaultLayout $ do
         --addStylesheet $ (StaticR css_home_css)
         [whamlet|
